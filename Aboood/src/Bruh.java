dae3bd3947c348ce3b7abf653f7d53051c194b85
TeaNTea123 <ahmed.almudarris@gmail.com>
2020-07-18 14:21:46 +0400

    Iterators

--- Aboood/src/Bruh.java
+++ Aboood/src/Bruh.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Bruh {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("C:\\Users\\ahmed\\Desktop\\no.txt"));
+        LinkedList<Integer> gr = new LinkedList<Integer>() ;
+        while (sc.hasNextInt()){
+            gr.push(sc.nextInt());
+        }
+        Iterator<Integer> IT = gr.iterator();
+        sc.close();
+        while (IT.hasNext()) {
+            System.out.println(IT.next());
+        }
+    }
 }
